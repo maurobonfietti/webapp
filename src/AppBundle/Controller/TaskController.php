91827e07129c6cf108fdb4c99df47c58c0edc2d5
Mauro Bonfietti <mauro.bonfietti@gmail.com>
2017-08-12 20:33:28 -0300

    Nuevo endpoint para buscar tareas.

--- src/AppBundle/Controller/TaskController.php
+++ src/AppBundle/Controller/TaskController.php
@@ -202,4 +202,77 @@ class TaskController extends Controller
         
         return $helpers->json($data);
     }
+    
+    public function searchAction(Request $request, $search = null)
+    {
+        $helpers = $this->get(Helpers::class);
+        $jwtAuth = $this->get(JwtAuth::class);
+
+        $token = $request->get('authorization', null);
+        $authCheck = $jwtAuth->checkToken($token);
+
+        if ($authCheck == true) {
+            $identity = $jwtAuth->checkToken($token, true);
+            
+            $em = $this->getDoctrine()->getManager();
+
+            $filter = $request->get('filter', null);
+            if (empty($filter)) {
+                $filter = null;
+            } elseif ($filter == 1) {
+                $filter = 'new';
+            } elseif ($filter == 2) {
+                $filter = 'todo';
+            } else {
+                $filter = 'finished';
+            }
+            
+            $order = $request->get('order', null);
+            if (empty($order) || $order == 2) {
+                $order = 'DESC';
+            } else {
+                $order = 'ASC';
+            }
+
+            if ($search != null) {
+                $dql = "
+                    SELECT t FROM BackendBundle:Task t WHERE t.user = $identity->sub
+                    AND t.title LIKE :search OR t.description LIKE :search
+                ";
+            } else {
+                $dql = "SELECT t FROM BackendBundle:Task t WHERE t.user = $identity->sub ";
+            }
+            
+            if ($filter != null) {
+                $dql.= " AND t.status = :filter ";
+            }
+            
+            $dql.= " ORDER BY t.id $order ";
+            
+            $query = $em->createQuery($dql);
+            
+            if (!empty($search)) {
+                $query->setParameter('search', "%$search%");
+            }
+            if ($filter != null) {
+                $query->setParameter('filter', "$filter");
+            }
+            
+            $task = $query->getResult();
+            
+            $data = [
+                'status' => 'success',
+                'code' => 200,
+                'data' => $task,
+            ];
+        } else {
+            $data = [
+                'status' => 'error',
+                'code' => 400,
+                'msg' => 'Authorization Invalid.',
+            ];
+        }
+        
+        return $helpers->json($data);
+    }
 }
