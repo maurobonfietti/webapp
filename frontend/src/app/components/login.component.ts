f898c23e6c47bd434fd093d67a843f686de537c9
Mauro Bonfietti <mauro.bonfietti@gmail.com>
2017-08-13 20:21:06 -0300

    Se agregan componentes login y register con angular.

---
+++ frontend/src/app/components/login.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+
+@Component({
+    selector: 'login',
+    templateUrl: '../views/login.html'
+})
+
+export class LoginComponent implements OnInit {
+    public title: string;
+
+    constructor() {
+        this.title = 'Componente de Login';
+    }
+
+    ngOnInit() {
+        console.log('El componente login.component ha sido cargado.');
+    }
+}
