f2dafec161f2e182acb08227557226cd070e80a1
Mauro Bonfietti <mauro.bonfietti@gmail.com>
2017-08-18 21:25:25 -0300

    Recibiendo los datos del formulario.

--- frontend/src/app/components/login.component.ts
+++ frontend/src/app/components/login.component.ts
@@ -8,12 +8,25 @@ import { Router, ActivatedRoute, Params } from '@angular/router';
 
 export class LoginComponent implements OnInit {
     public title: string;
+    public user;
 
-    constructor() {
+    constructor(
+        private _route: ActivatedRoute,
+        private _router: Router
+    ) {
         this.title = 'Ingresar:';
+        this.user = {
+            "email": "",
+            "password": "",
+            "gethash": "false"
+        };
     }
 
     ngOnInit() {
         console.log('El componente login.component ha sido cargado.');
+    }
+
+    onSubmit() {
+        console.log(this.user);
     }
 }
