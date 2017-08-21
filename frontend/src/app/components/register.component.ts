7cc6186be0120e127c0a6aecf462a20efad0a7f3
Mauro Bonfietti <mauro.bonfietti@gmail.com>
2017-08-20 22:45:12 -0300

    Recibir datos del formu de creacion de usuarios.

--- frontend/src/app/components/register.component.ts
+++ frontend/src/app/components/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, Params } from '@angular/router';
+import { User } from '../models/user';
 
 @Component({
     selector: 'register',
@@ -8,15 +9,21 @@ import { Router, ActivatedRoute, Params } from '@angular/router';
 
 export class RegisterComponent implements OnInit {
     public title: string;
+    public user: User;
 
     constructor (
         private _route: ActivatedRoute,
         private _router: Router
     ) {
         this.title = 'Registrate';
+        this.user = new User(1, "user", "", "", "", "");
     }
 
     ngOnInit() {
         console.log('El componente register.component ha sido cargado.');
+    }
+
+    onSubmit() {
+        console.log(this.user);
     }
 }
