1375d3e09d839f60ebd8a7bc6dd33564d3813dc2
Mauro Bonfietti <mauro.bonfietti@gmail.com>
2017-09-10 19:16:12 -0300

    Primeros tests y assertions para servicios de listar tareas.

--- backend/tests/AppBundle/Controller/WebAppTest.php
+++ backend/tests/AppBundle/Controller/WebAppTest.php
@@ -7,22 +7,46 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 class ApplicationAvailabilityFunctionalTest extends WebTestCase
 {
     /**
-     * @dataProvider urlProvider
+     * Url of endpoints to test.
+     *
+     * @return array
      */
-    public function testPageIsSuccessful($url)
-    {
-        $client = self::createClient();
-        $client->request('POST', $url);
-
-        $this->assertTrue($client->getResponse()->isSuccessful());
-    }
-
     public function urlProvider()
     {
         return array(
             array('/task/list'),
             array('/task/detail/12'),
-//            array('/post/fixture-post-1'),
         );
+    }
+
+    /**
+     * @dataProvider urlProvider
+     */
+    public function testPageIsSuccessful($url)
+    {
+        $client = self::createClient();
+        $crawler = $client->request('POST', $url);
+
+        $this->assertTrue($client->getResponse()->isSuccessful());
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+//        $this->assertContains('200', $client->getResponse()->getContent());
+//        $this->assertNotContains('Authorization Invalid', $client->getResponse()->getContent());
+//        $this->assertNotContains('error', $client->getResponse()->getContent());
+//        $this->assertNotContains('400', $client->getResponse()->getContent());
+    }
+
+    /**
+     * @dataProvider urlProvider
+     */
+    public function testPageIsNotAllowed($url)
+    {
+        $client = self::createClient();
+        $client->request('POST', $url);
+
+        $this->assertContains('Authorization Invalid', $client->getResponse()->getContent());
+        $this->assertContains('error', $client->getResponse()->getContent());
+        $this->assertContains('400', $client->getResponse()->getContent());
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertTrue($client->getResponse()->isSuccessful());
     }
 }
