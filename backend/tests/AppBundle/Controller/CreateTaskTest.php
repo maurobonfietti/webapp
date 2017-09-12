9ad0fff9a952b215f11d05e163597c5293bc8c53
Mauro Bonfietti <mauro.bonfietti@gmail.com>
2017-09-12 00:59:36 -0300

    Agrego tests con caso de exito y error, al crear tareas. Code Coverage: >90%.

---
+++ backend/tests/AppBundle/Controller/CreateTaskTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tests\AppBundle;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class CreateTaskTest extends WebTestCase
+{
+    public function testCreateTaskOk()
+    {
+        $data = [
+            'authorization' => 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOjEzLCJlbWFpbCI6Im1AYi5jb20uYXIiLCJuYW1lIjoiTWF1cml0byIsInN1cm5hbWUiOiJCb25kIiwiaWF0IjoxNTA1MDc3NDQ2LCJleHAiOjE1MDU2ODIyNDZ9.VP6hyBPMCyzcYg5wnlQPVaFi85xjMo3un9etU4NETPY',
+            'json' => '{"title":"test.", "description":"Mi test 1...", "status":"todo"}',
+        ];
+        $client = self::createClient();
+        $client->request('POST', '/task/new', $data);
+        $this->assertTrue($client->getResponse()->isSuccessful());
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertContains('success', $client->getResponse()->getContent());
+        $this->assertContains('Task Created.', $client->getResponse()->getContent());
+        $this->assertNotContains('error', $client->getResponse()->getContent());
+        $this->assertNotContains('Authorization Invalid', $client->getResponse()->getContent());
+    }
+
+    public function testCreateTaskError()
+    {
+        $client = self::createClient();
+        $client->request('POST', '/task/new');
+        $this->assertTrue($client->getResponse()->isSuccessful());
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertContains('Authorization Invalid', $client->getResponse()->getContent());
+        $this->assertNotContains('success', $client->getResponse()->getContent());
+    }
+}
