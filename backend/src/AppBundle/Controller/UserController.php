f3f14b21290e3cf50fec407addc7398a4cb39667
Mauro Bonfietti <mauro.bonfietti@gmail.com>
2017-09-16 19:10:52 -0300

    Creo clase UserService.

--- backend/src/AppBundle/Controller/UserController.php
+++ backend/src/AppBundle/Controller/UserController.php
@@ -4,51 +4,20 @@ namespace AppBundle\Controller;
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Validator\Constraints as Assert;
-use AppBundle\Entity\Users;
 use AppBundle\Services\Helpers;
 use AppBundle\Services\JwtAuth;
+use AppBundle\Services\UserService;
 
 class UserController extends BaseController
 {
     public function newAction(Request $request)
     {
         try {
-            $json = $request->get('json', null);
-            $params = json_decode($json);
-            $createdAt = new \DateTime("now");
-            $role = 'user';
-            $email = isset($params->email) ? $params->email : null;
-            $name = isset($params->name) ? $params->name : null;
-            $surname = isset($params->surname) ? $params->surname : null;
-            $password = isset($params->password) ? $params->password : null;
-            $emailConstraint = new Assert\Email();
-            $validateEmail = $this->get('validator')->validate($email, $emailConstraint);
-            if ($email == null || count($validateEmail) > 0 || $name == null || $password == null || $surname == null) {
-                throw new \Exception('error: User Not Created.....!', 400);
-            }
-            $user = new Users();
-            $user->setCreatedAt($createdAt);
-            $user->setRole($role);
-            $user->setEmail($email);
-            $user->setName($name);
-            $user->setSurname($surname);
-            $pwd = hash('sha256', $password);
-            $user->setPassword($pwd);
             $em = $this->getDoctrine()->getManager();
-            $issetUser = $em->getRepository('AppBundle:Users')->findBy(["email" => $email]);
-            if (count($issetUser) == 0) {
-                $em->persist($user);
-                $em->flush();
-                $status = 200;
-                $data = [
-                    'status' => 'success',
-                    'code' => $status,
-                    'msg' => 'User Created.',
-                    'user' => $user,
-                ];
-            } else {
-                throw new \Exception('error: User exists.', 400);
-            }
+            $json = $request->get('json', null);
+            $validator = $this->get('validator');
+            $userService = new UserService();
+            $data = $userService->create($json, $validator, $em);
 
             return $this->get(Helpers::class)->json($data);
         } catch (\Exception $e) {
