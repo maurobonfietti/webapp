a56afc4a4da1edaa43a3e33725da7c1ee7c917ef
Mauro Bonfietti <mauro.bonfietti@gmail.com>
2017-10-02 21:45:55 -0300

    Retoque en tests de login.

--- backend/tests/Functional/AsLoginTest.php
+++ backend/tests/Functional/AsLoginTest.php
@@ -12,12 +12,14 @@ class AsLoginTest extends BaseTest
         $client = self::createClient();
         $client->request('POST', '/login', $data);
 
-        self::$bearer = substr($client->getResponse()->getContent(), 1, -1);
+        $result = $client->getResponse()->getContent();
+        self::$bearer = substr($result, 1, -1);
 
         $this->assertTrue($client->getResponse()->isSuccessful());
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        $this->assertContains('ey', $client->getResponse()->getContent());
-        $this->assertNotContains('error', $client->getResponse()->getContent());
+
+        $this->assertContains('ey', $result);
+        $this->assertNotContains('error', $result);
     }
 
     public function testLoginHashOk()
@@ -27,8 +29,10 @@ class AsLoginTest extends BaseTest
         ];
         $client = self::createClient();
         $client->request('POST', '/login', $data);
+
         $this->assertTrue($client->getResponse()->isSuccessful());
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
+
         $this->assertContains('sub', $client->getResponse()->getContent());
         $this->assertContains('email', $client->getResponse()->getContent());
         $this->assertContains('name', $client->getResponse()->getContent());
@@ -45,7 +49,9 @@ class AsLoginTest extends BaseTest
         ];
         $client = self::createClient();
         $client->request('POST', '/login', $data);
+
         $this->assertEquals(403, $client->getResponse()->getStatusCode());
+
         $this->assertContains('error', $client->getResponse()->getContent());
         $this->assertContains('incorrecto', $client->getResponse()->getContent());
         $this->assertNotContains('ey', $client->getResponse()->getContent());
