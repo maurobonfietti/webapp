41dd1c4ea16e75647958f4b22c10fafc37b50ec3
Mauro Bonfietti <mauro.bonfietti@gmail.com>
2017-10-14 03:46:38 -0300

    Habilito metodo PATCH en CORS Headers.

--- backend/web/app_dev.php
+++ backend/web/app_dev.php
@@ -21,7 +21,7 @@ if (isset($_SERVER['HTTP_CLIENT_IP'])
 // Enable HTTP CORS Headers.
 header('Access-Control-Allow-Origin: *');
 header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method, Authorization");
-header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+header("Access-Control-Allow-Methods: GET, POST, PATCH, OPTIONS, PUT, DELETE");
 header("Allow: GET, POST, OPTIONS, PUT, DELETE");
 $method = $_SERVER['REQUEST_METHOD'];
 if($method == "OPTIONS") {
