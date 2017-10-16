2979e816823552b20b9be21b341566c7d3d07c3c
Mauro Bonfietti <mauro.bonfietti@gmail.com>
2017-10-15 22:41:52 -0300

    Improvements on User Service.

--- backend/src/AppBundle/Services/UserService.php
+++ backend/src/AppBundle/Services/UserService.php
@@ -33,19 +33,25 @@ class UserService
         $name = isset($params->name) ? $params->name : null;
         $surname = isset($params->surname) ? $params->surname : null;
         $password = isset($params->password) ? $params->password : null;
-        $emailConstraint = new Assert\Email();
-        $validateEmail = $this->validator->validate($email, $emailConstraint);
-        if ($email === null || count($validateEmail) > 0 || $name === null || $password === null || $surname === null) {
-            throw new \Exception('error: Usuario no creado.', 400);
-        }
-        $checkUserExist = $this->em->getRepository('AppBundle:Users')->findBy(["email" => $email]);
-        if (count($checkUserExist) >= 1) {
-            throw new \Exception('error: Usuario existente.', 400);
-        }
+
+        $this->validateCreateUser($email, $name, $surname, $password);
         $userRepository = new UserRepository($this->em);
         $user = $userRepository->create($email, $name, $surname, $password);
 
         return $user;
+    }
+
+    private function validateCreateUser($email, $name, $surname, $password)
+    {
+        $emailConstraint = new Assert\Email();
+        $validateEmail = $this->validator->validate($email, $emailConstraint);
+        if ($email === null || $name === null || $surname === null || $password === null || count($validateEmail) > 0 ) {
+            throw new \Exception('error: Usuario no creado.', 400);
+        }
+        $checkUserExist = $this->em->getRepository('AppBundle:Users')->findBy(["email" => $email]);
+        if ($checkUserExist) {
+            throw new \Exception('error: Usuario existente.', 400);
+        }
     }
 
     public function update($token, $json)
@@ -56,19 +62,26 @@ class UserService
         $name = isset($params->name) ? $params->name : null;
         $surname = isset($params->surname) ? $params->surname : null;
         $password = isset($params->password) ? $params->password : null;
+
+        $user = $this->getAndValidateUser($email, $name, $surname, $identity);
+        $userRepository = new UserRepository($this->em);
+        $data = $userRepository->update($user, $email, $name, $surname, $password);
+
+        return $data;
+    }
+
+    private function getAndValidateUser($email, $name, $surname, $identity)
+    {
         $emailConstraint = new Assert\Email();
         $validateEmail = $this->validator->validate($email, $emailConstraint);
         if ($email === null || count($validateEmail) > 0 || $name === null || $surname === null) {
             throw new \Exception('error: Usuario no actualizado.', 400);
         }
-        $checkUserExist = $this->em->getRepository('AppBundle:Users')->findBy(["email" => $email]);
-        if (count($checkUserExist) >= 1 && $identity->email != $email) {
+        $checkUserExist = $this->em->getRepository('AppBundle:Users')->findOneBy(["email" => $email]);
+        if ($checkUserExist && $identity->email !== $email) {
             throw new \Exception('error: Usuario existente.', 400);
         }
-        $user = $this->em->getRepository('AppBundle:Users')->findOneBy(["id" => $identity->sub]);
-        $userRepository = new UserRepository($this->em);
-        $data = $userRepository->update($user, $email, $name, $surname, $password);
 
-        return $data;
+        return $this->em->getRepository('AppBundle:Users')->findOneBy(["id" => $identity->sub]);
     }
 }
