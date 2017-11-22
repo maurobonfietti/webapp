675d96a188400ba50ea720acbe53cfcea31bb4dd
Mauro Bonfietti <mauro.bonfietti@gmail.com>
2017-11-21 21:38:21 -0300

    Auto-Indent frontend app files.

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,43 +1,43 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { HttpModule } from '@angular/http';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {HttpModule} from '@angular/http';
+import {NgModule} from '@angular/core';
 
-import { FormsModule, ReactiveFormsModule }   from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
-import { routing, appRoutingProviders} from './app.routing';
+import {routing, appRoutingProviders} from './app.routing';
 
-import { AppComponent } from './app.component';
-import { LoginComponent } from './components/login.component';
-import { RegisterComponent } from './components/register.component';
-import { DefaultComponent } from './components/default.component';
-import { UserEditComponent } from './components/user.edit.component';
-import { TaskNewComponent } from './components/task.new.component';
-import { TaskDetailComponent } from './components/task.detail.component';
-import { TaskEditComponent } from './components/task.edit.component';
-import { GenerateDatePipe } from './pipes/generate.date.pipe';
+import {AppComponent} from './app.component';
+import {LoginComponent} from './components/login.component';
+import {RegisterComponent} from './components/register.component';
+import {DefaultComponent} from './components/default.component';
+import {UserEditComponent} from './components/user.edit.component';
+import {TaskNewComponent} from './components/task.new.component';
+import {TaskDetailComponent} from './components/task.detail.component';
+import {TaskEditComponent} from './components/task.edit.component';
+import {GenerateDatePipe} from './pipes/generate.date.pipe';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    LoginComponent,
-    RegisterComponent,
-    DefaultComponent,
-    UserEditComponent,
-    TaskNewComponent,
-    TaskDetailComponent,
-    TaskEditComponent,
-    GenerateDatePipe,
-  ],
-  imports: [
-    routing,
-    BrowserModule,
-    HttpModule,
-    FormsModule,
-    ReactiveFormsModule
-  ],
-  providers: [
-    appRoutingProviders
-  ],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        LoginComponent,
+        RegisterComponent,
+        DefaultComponent,
+        UserEditComponent,
+        TaskNewComponent,
+        TaskDetailComponent,
+        TaskEditComponent,
+        GenerateDatePipe,
+    ],
+    imports: [
+        routing,
+        BrowserModule,
+        HttpModule,
+        FormsModule,
+        ReactiveFormsModule
+    ],
+    providers: [
+        appRoutingProviders
+    ],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
