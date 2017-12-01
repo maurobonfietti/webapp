a37f55cd2c5eb23107f20dd072107bb50d4fdd7b
Mauro Bonfietti <mauro.bonfietti@gmail.com>
2017-11-30 23:02:53 -0300

    Unit test for check delete a task not found.

--- backend/tests/Functional/Task/DeleteTaskTest.php
+++ backend/tests/Functional/Task/DeleteTaskTest.php
@@ -26,4 +26,16 @@ class DeleteTaskTest extends BaseTest
         $this->assertContains('Sin Autorizacion', $client->getResponse()->getContent());
         $this->assertNotContains('success', $client->getResponse()->getContent());
     }
+
+    public function testDeleteTaskNotFound()
+    {
+        $client = self::createClient();
+        $client->request('DELETE', '/task/remove/1234567890', [], [], [
+            'HTTP_authorization' => $this->getAuthToken(),
+        ]);
+
+        $this->assertEquals(404, $client->getResponse()->getStatusCode());
+        $this->assertContains('Task not found', $client->getResponse()->getContent());
+        $this->assertNotContains('success', $client->getResponse()->getContent());
+    }
 }
