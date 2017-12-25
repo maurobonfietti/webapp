63438dc4870b0013f7af6cb5143b36c53dc7c0c5
Mauro Bonfietti <mauro.bonfietti@gmail.com>
2017-12-25 16:15:17 -0300

    Added tests for the new service to change the status of a task.

---
+++ backend/tests/Functional/Task/UpdateStatusTaskTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests\Functional;
+
+class UpdateStatusTaskTest extends BaseTest
+{
+    public function testUpdateTaskOk()
+    {
+        $client = self::createClient();
+        $client->request(
+            'PATCH', '/task/update-status/773', 
+            [], 
+            [], 
+            ['HTTP_authorization' => $this->getAuthToken()]
+        );
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertTrue($client->getResponse()->isSuccessful());
+        $this->assertContains('success', $client->getResponse()->getContent());
+        $this->assertNotContains('error', $client->getResponse()->getContent());
+        $this->assertNotContains('not authorized', $client->getResponse()->getContent());
+    }
+}
