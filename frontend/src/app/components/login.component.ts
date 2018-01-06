3c7b6cd97ace5016a9b076bc7a9b66998f84d5c8
Mauro Bonfietti <mauro.bonfietti@gmail.com>
2018-01-06 12:21:30 -0300

    Continuing the material design on the new branch.

--- frontend/src/app/components/login.component.ts
+++ frontend/src/app/components/login.component.ts
@@ -119,7 +119,7 @@ export class LoginComponent implements OnInit {
                                 } {
                                     if (!this.token.status) {
                                         localStorage.setItem('token', JSON.stringify(this.token));
-//                                        this.openSnackBar();
+                                        this.openSnackBar();
                                         window.location.href = '/index/1';
                                     }
                                 }
