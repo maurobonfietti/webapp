dadcdb74983ab91e689101e42d70c2333ec0c4d0
Mauro Bonfietti <mauro.bonfietti@gmail.com>
2018-01-17 00:25:46 -0300

    1 intento mas y a dormir ja.

--- backend/web/app.php
+++ backend/web/app.php
@@ -13,7 +13,8 @@ if($method == "OPTIONS") {
 }
 
 /** @var \Composer\Autoload\ClassLoader $loader */
-$loader = require __DIR__.'/../vendor/autoload.php';
+//$loader = require __DIR__.'/../vendor/autoload.php';
+$loader = require '/app/backend/web/../vendor/autoload.php';
 if (PHP_VERSION_ID < 70000) {
     include_once __DIR__.'/../var/bootstrap.php.cache';
 }
